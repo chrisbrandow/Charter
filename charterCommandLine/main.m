0caf65bf42c27d5776dcc14b0a85de16afb63016
chrisbrandow <chrisbrandow@gmail.com>
2014-08-19 22:09:25 -0700

    Initial Commit

---
+++ charterCommandLine/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  charterCommandLine
+//
+//  Created by Christopher Brandow on 8/19/14.
+//  Copyright (c) 2014 Flouu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
